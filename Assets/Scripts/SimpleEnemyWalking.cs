2ee967131b7becd5e45f6bac12d0b1f9fb6df9a2
neohazardous <ol29@njit.edu>
2020-03-06 15:53:57 -0500

    Added some extra scripts.

---
+++ Assets/Scripts/SimpleEnemyWalking.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//** PLEASE NOTE THIS IS FOR 2D SPACE ONLY **//
+
+public class SimpleEnemyWalking : MonoBehaviour
+{
+
+    
+    public GameObject enemy;
+    public Transform[] WayPoints;
+    public int currentWayPoint;
+    public float Speed = 1.0f;
+    public Collider attackRange;
+    private SpriteRenderer renderer;
+    private Material mat;
+
+    public Renderer rend;
+    // Use this for initialization
+    void Start()
+    {
+        renderer = enemy.GetComponent<SpriteRenderer>();
+        mat = enemy.GetComponent<Material>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //If im at waypoint A, then increase the currentWayPoint so that I move to the next WayPoint
+        if (transform.position == WayPoints[currentWayPoint].position)
+        {
+            currentWayPoint++;
+            renderer.flipX = true;
+        }
+
+        //If Im at waypoint B, reset so I go back to waypoint A
+        if (currentWayPoint >= WayPoints.Length)
+        {
+            currentWayPoint = 0;
+            renderer.flipX = false;
+        }
+       
+        //heres teh actual movement
+        transform.position = Vector3.MoveTowards(transform.position, WayPoints[currentWayPoint].position, Speed * Time.deltaTime);
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Debug.Log("Boop.");
+    }
+}
---
+++ Assets/Scripts/textboxTrig.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class textboxTrig : MonoBehaviour {
+    [SerializeField]
+    Canvas messageCanvas; //Make the object in the script
+
+    // Use this for initialization
+    void Start()
+    {
+        messageCanvas.enabled = false; //this makes the canvas disappear
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.name == "Player")
+        { //if a gameobject by the name of "Player" touches the collider
+            TurnOnMessage(); //call the TurnOnMessage() function
+        }
+    }
+
+    private void TurnOnMessage()
+    {
+        messageCanvas.enabled = true; //make the canvas appear
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.name == "Player")
+        { //if a gameobject by the name of Vinny is NOT touching the collider
+            TurnOffMessage(); //call the TurnOffMessage() function
+        }
+    }
+    private void TurnOffMessage()
+    {
+        messageCanvas.enabled = false; //make the canvas disappear
+    }
+}
